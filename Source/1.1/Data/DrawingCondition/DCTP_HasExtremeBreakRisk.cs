4081244d1cf9a97ca3f3a568b6edb4388ed8bbe2
TammyBee <tammybeetmb@gmail.com>
2020-03-29 17:35:28 +0900

    fix DCTP_HasXXXBreakLisk copy bug.

--- Source/1.1/Data/DrawingCondition/DCTP_HasExtremeBreakRisk.cs
+++ Source/1.1/Data/DrawingCondition/DCTP_HasExtremeBreakRisk.cs
@@ -21,7 +21,7 @@ namespace DetailPortraits.Data.DrawingCondition {
 
         public override DrawingConditionTermPreset Copy {
             get {
-                return new DCTP_HasMinorBreakRisk();
+                return new DCTP_HasExtremeBreakRisk();
             }
         }
 
